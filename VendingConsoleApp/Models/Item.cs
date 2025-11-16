525ce9d058622c2bc36719daad3abbeecdb1ef29
Анастасия Рожнова <nastyarozhnova@icloud.com>
2025-11-16 23:25:35 +0300

    pravki

---
+++ VendingConsoleApp/Models/Item.cs
@@ -0,0 +1,18 @@
+namespace VendingConsoleApp
+{
+    public sealed class Item
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Item(int id, string name, decimal price, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ VendingConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VendingConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            var vm = new VendingMachine();
+            vm.Run();
+        }
+    }
+}
